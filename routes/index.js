25468b4c7f46b8593f8a0ef9f93e1889e248517f
Arup Kumar Maiti <30426515+sloppy-genius@users.noreply.github.com>
2017-10-02 21:45:43 +0530

    0.01__added_homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'spOOn' });
+  res.render('index', {
+    title: 'spOOn',
+    author: 'sloppy-genius' });
 });
 
 module.exports = router;
